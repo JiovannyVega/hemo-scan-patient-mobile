cbc9e2da79cb8108bb486d0c8ea8724313aa317d
feralcojam <feral.cojam@gmail.com>
2025-03-26 15:57:36 -0600

    Diseño: cambio de color de fondo; estilar por completo la pantalla Datos

--- screens/datos/DatosScreen.styles.js
+++ screens/datos/DatosScreen.styles.js
@@ -3,35 +3,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#1B595D',
+        backgroundColor: '#EDF1F2',
     },
-    dataView: {
-        flex: 2,
-        flexDirection: 'row',
-        backgroundColor: '#184666',
-        borderBottomLeftRadius: 25,
-        borderBottomRightRadius: 25,
-        paddingTop: 20,
-    },
-    label: {
-        color: '#F7F7F7',
-        fontWeight: '500',
-        lineHeight: 30,
-    },
-    userLabel: {
-        color: '#F7F7F7',
-        lineHeight: 30,
-    },
-    labelView: {
-        flex: 1,
-        paddingLeft: 50,
-    },
-    userInfoView: {
-        flex: 1,
-        alignItems: 'flex-end',
-        paddingRight: 50,  
-    },
-    bodyView: {
-        flex: 4,
-    }
 });
